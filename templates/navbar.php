2da3b39cef3896d1a04cb84b27adfb9c86f02d6e
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-13 14:33:22 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -22,6 +22,7 @@ if (isset($_SESSION['user_id'])){
 		$_SESSION['profile_picture'] = $profilePicture; // Set session variable
 	}
 }
+echo($_SESSION['profile_picture']);
 ?>
 
 <!-- Navbar Section -->
@@ -58,7 +59,7 @@ if (isset($_SESSION['user_id'])){
 
 		<div class="navbar-item">
         <a href="/app/account_page.php" class="navbar-item" style="width: 48px; height: 48px; display: flex; align-items: center; justify-content: center;">
-            <img src="/<?php htmlspecialchars($_SESSION['profile_picture']); ?>" alt="Profile Picture" style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+            <img src="<?php echo htmlspecialchars($_SESSION['profile_picture']); ?>" alt="Profile Picture" style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
         </a>
 
 
