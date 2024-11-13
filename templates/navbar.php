5300cc828ec213ffcf447d9486d8c71da3a82bc2
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-13 14:37:53 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -57,15 +57,13 @@ echo($_SESSION['profile_picture']);
             </div>
         </form>
 
-		<div class="navbar-item">
-        <a href="/app/account_page.php" style="width: 48px; height: 48px; display: flex; align-items: center; justify-content: center;">
-    <img src="<?php echo htmlspecialchars($_SESSION['profile_picture']); ?>" alt="Profile Picture" style="width: 100%; height: 100%; object-fit: cover; object-position: center; border-radius: 50%;">
-</a>
-
-
-
-
-
+		<div>
+        <a href="/app/account_page.php" style="width: 48px; height: 100%; display: flex; align-items: center; justify-content: center;">
+            <img src="<?php echo htmlspecialchars($_SESSION['profile_picture']); ?>" alt="Profile Picture" style="width: 100%; height: 100%; object-fit: cover; object-position: center;">
+        </a>
+        </div>
+        
+        <div class="navbar-item">
             <?php if (isset($_SESSION['user_id'])): ?>
                 <a href="/app/account_page.php">Account</a>
                 <a href="/Scripts/logout.php">Log Out</a>
