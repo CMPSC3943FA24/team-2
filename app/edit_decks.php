a818a30906e0e9c464b029ac546aef5ba73865fe
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-18 21:14:56 -0600

    Separated viewing and editing decks

--- app/edit_decks.php
+++ app/edit_decks.php
@@ -127,49 +127,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['action'], $_GET['deck_id
                 <button class="button is-primary" type="submit">Add Cards</button>
             </div>
         </form>
-        <div class="box">
-            <h2 class="subtitle">View Deck</h2>
-            <div class="select">
-                <select name="deck_id" id="deck_id" required>
-                    <option value="">Select a Deck</option>
-                    <?php
-                    // Fetch all decks from the database
-                    $result = $conn->query("SELECT deck_id, deck_name FROM decks");
-                    while ($deck = $result->fetch_assoc()) {
-                        echo "<option value=\"{$deck['deck_id']}\">{$deck['deck_name']}</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-
-            <div id="card_list">
-                <!-- The card list will be dynamically updated here -->
-            </div>
-        </div>
-
-        <script>
-        // JavaScript to handle AJAX request
-        document.getElementById('deck_id').addEventListener('change', function() {
-            var deck_id = this.value;
-
-            if (deck_id) {
-                // Send AJAX request to fetch cards based on the selected deck
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', 'fetch_cards.php?deck_id=' + deck_id, true);
-                xhr.onload = function() {
-                    if (xhr.status === 200) {
-                        // Update the card list with the response
-                        document.getElementById('card_list').innerHTML = xhr.responseText;
-                    }
-                };
-                xhr.send();
-            } else {
-                document.getElementById('card_list').innerHTML = ''; // Clear card list if no deck is selected
-            }
-        });
-        </script>
-
-
     </div>
 </div>
 </body>
