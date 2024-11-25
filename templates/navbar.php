364b2ffdb4aea995fa1abbd69248af5109825044
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-25 11:59:00 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -45,20 +45,21 @@ if (isset($_SESSION['user_id'])){
         </div>
 
         <!-- Profile Picture and Logout Button -->
-        <div class="navbar-item" style="display: flex; align-items: center; justify-content: center;">
-            <a href="/app/account_page.php" style="display: block; width: 48px; height: 48px; overflow: hidden;">
+        <div class="navbar-item">
+            <a href="/app/account_page.php">
                 <?php
                 // Check if the session variable is set and not null
                 if (isset($_SESSION['profile_picture']) && !empty($_SESSION['profile_picture'])) {
                     $profilePicture = htmlspecialchars($_SESSION['profile_picture']);
-                    echo '<img src="' . $profilePicture . '?' . time() . '" alt="Profile Picture" style="object-fit: cover; width: 100%; height: 100%;">';
+                    echo '<figure class="image is-48x48 is-rounded"><img src="' . $profilePicture . '?' . time() . '" alt="Profile Picture"></figure>';
                 } else {
                     // Default image if no profile picture is found
-                    echo '<img src="/images/account.jpg" alt="Default Profile Picture" style="object-fit: cover; width: 100%; height: 100%;">';
+                    echo '<figure class="image is-48x48 is-rounded"><img src="/images/account.jpg" alt="Default Profile Picture"></figure>';
                 }
                 ?>
             </a>
         </div>
+
 
 
 
