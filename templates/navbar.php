c3a0c89fa9a706b06a2a6d61876958c55dac07cd
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-25 12:15:56 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -63,7 +63,7 @@ if (isset($_SESSION['user_id'])){
             </a>
         </div>
 
-        <div class="navbar-item"><figure class = "image is-32x32"><img src="/uploads/2_profile.jpg"></img></figure></div>
+        <div class="navbar-item"><img src="/uploads/2_profile.jpg"></img></figure></div>
 
 
 
