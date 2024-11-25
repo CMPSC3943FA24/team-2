154b2ec66ffc420bbabd9d6799dc1edfd5518ee4
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-25 12:06:49 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -55,6 +55,8 @@ if (isset($_SESSION['user_id'])){
             </a>
         </div>
 
+        <div class="navbar-item"><figure class = "image is-48x48"><img src="/uploads/2_profile.jpg"></img></figure></div>
+
 
 
 
