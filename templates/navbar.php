649b8a0d718da066a85e200d3fa580c24b8456c3
Michael Crowell <118855305+mgcrowell@users.noreply.github.com>
2024-11-25 11:11:30 -0600

    Update navbar.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['user_id'])){
 ?>
 
 <!-- Navbar Section -->
-<nav class="navbar is-light">
+<nav class="navbar">
     <div class="navbar-brand">
         <a href="/" class="navbar-item" id="main">
             <strong>CARDSTOCK</strong>
